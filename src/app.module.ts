6bf3529084e6e1e0e4360cddb0143673ca7cf6d0
Glen Perera <123937629+jumangl@users.noreply.github.com>
2024-03-21 15:05:42 +0530

    Added Modules for Owner and the animal

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { NinjasModule } from './ninjas/ninjas.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [NinjasModule],
+  imports: [NinjasModule, MongooseModule.forRoot('mongodb://localhost/net')],
   controllers: [AppController],
   providers: [AppService],
 })
